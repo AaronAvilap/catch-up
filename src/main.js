13090bb4daec74dda244622e333e2b3cdbd96451
Aarón Alexander Avila Palacios <u201823654@upc.edu.pe>
2026-04-30 18:00:26 -0500

    feat(material-design-ui): add the entire project

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Material from '@primeuix/themes/material';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import {Avatar, Button, Card, Drawer, Image, Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
-
+import i18n from "./i18n.js";
 
 createApp(App)
     .use(i18n)
